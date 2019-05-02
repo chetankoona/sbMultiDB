52c80ef77fd7ec5f92683aac473888f8576cc638
Chetan  k <chetan.k@tavant.com>
2019-05-02 17:39:34 +0530

    addedd properties value

--- src/main/java/com/example/springbootapp/SampleRestController.java
+++ src/main/java/com/example/springbootapp/SampleRestController.java
@@ -2,13 +2,17 @@ package com.example.springbootapp;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class SampleRestController {
 	
+	@Value("propertyone")
+	private String propertyone;
+	
 	@RequestMapping("/")
 	public String displayString() {
-		return "Application on openshift";
+		return "Application on openshift "+ propertyone;
 	}
 
 }
